99a9cc0e744f7fd98663f487dd79ad99eb40880f
mhoveida <mohaddesehhoveida@yahoo.com>
2026-05-13 16:34:15 -0400

    Initial commit

---
+++ extract_training_data.py
@@ -0,0 +1,206 @@
+from conll_reader import DependencyStructure, conll_reader
+from collections import defaultdict
+import copy
+import sys
+import numpy as np
+
+class State(object):
+    def __init__(self, sentence = []):
+        self.stack = []
+        self.buffer = []
+        if sentence: 
+            self.buffer = list(reversed(sentence))
+        self.deps = set() 
+    
+    def shift(self):
+        self.stack.append(self.buffer.pop())
+
+    def left_arc(self, label):
+        self.deps.add( (self.buffer[-1], self.stack.pop(),label) )
+
+    def right_arc(self, label):
+        parent = self.stack.pop()
+        self.deps.add( (parent, self.buffer.pop(), label) )
+        self.buffer.append(parent)
+
+    def __repr__(self):
+        return "{},{},{}".format(self.stack, self.buffer, self.deps)
+
+   
+
+def apply_sequence(seq, sentence):
+    state = State(sentence)
+    for rel, label in seq:
+        if rel == "shift":
+            state.shift()
+        elif rel == "left_arc":
+            state.left_arc(label) 
+        elif rel == "right_arc":
+            state.right_arc(label) 
+         
+    return state.deps
+   
+class RootDummy(object):
+    def __init__(self):
+        self.head = None
+        self.id = 0
+        self.deprel = None    
+    def __repr__(self):
+        return "<ROOT>"
+
+     
+def get_training_instances(dep_structure):
+
+    deprels = dep_structure.deprels
+    
+    sorted_nodes = [k for k,v in sorted(deprels.items())]
+    state = State(sorted_nodes)
+    state.stack.append(0)
+
+    childcount = defaultdict(int)
+    for ident,node in deprels.items():
+        childcount[node.head] += 1
+ 
+    seq = []
+    while state.buffer: 
+        if not state.stack:
+            seq.append((copy.deepcopy(state),("shift",None)))
+            state.shift()
+            continue
+        if state.stack[-1] == 0:
+            stackword = RootDummy() 
+        else:
+            stackword = deprels[state.stack[-1]]
+        bufferword = deprels[state.buffer[-1]]
+        if stackword.head == bufferword.id:
+            childcount[bufferword.id]-=1
+            seq.append((copy.deepcopy(state),("left_arc",stackword.deprel)))
+            state.left_arc(stackword.deprel)
+        elif bufferword.head == stackword.id and childcount[bufferword.id] == 0:
+            childcount[stackword.id]-=1
+            seq.append((copy.deepcopy(state),("right_arc",bufferword.deprel)))
+            state.right_arc(bufferword.deprel)
+        else: 
+            seq.append((copy.deepcopy(state),("shift",None)))
+            state.shift()
+    return seq   
+
+
+dep_relations = ['tmod', 'vmod', 'csubjpass', 'rcmod', 'ccomp', 'poss', 'parataxis', 'appos', 'dep', 'iobj', 'pobj', 'mwe', 'quantmod', 'acomp', 'number', 'csubj', 'root', 'auxpass', 'prep', 'mark', 'expl', 'cc', 'npadvmod', 'prt', 'nsubj', 'advmod', 'conj', 'advcl', 'punct', 'aux', 'pcomp', 'discourse', 'nsubjpass', 'predet', 'cop', 'possessive', 'nn', 'xcomp', 'preconj', 'num', 'amod', 'dobj', 'neg','dt','det']
+
+
+class FeatureExtractor(object):
+       
+    def __init__(self, word_vocab_file, pos_vocab_file):
+        self.word_vocab = self.read_vocab(word_vocab_file)        
+        self.pos_vocab = self.read_vocab(pos_vocab_file)        
+        self.output_labels = self.make_output_labels()
+
+    def make_output_labels(self):
+        labels = []
+        labels.append(('shift',None))
+    
+        for rel in dep_relations:
+            labels.append(("left_arc",rel))
+            labels.append(("right_arc",rel))
+        return dict((label, index) for (index,label) in enumerate(labels))
+
+    def read_vocab(self,vocab_file):
+        vocab = {}
+        for line in vocab_file: 
+            word, index_s = line.strip().split()
+            index = int(index_s)
+            vocab[word] = index
+        return vocab     
+
+    def get_input_representation(self, words, pos, state):
+        # TODO: Write this method for Part 2
+        input_vector = np.zeros(6)
+        #mapping special symbols to their indices
+        special_symbols = {
+        'CD': self.word_vocab['<CD>'],
+        'NNP': self.word_vocab['<NNP>'],
+        'UNK': self.word_vocab['<UNK>'],
+        'ROOT': self.word_vocab['<ROOT>'], 
+        'NULL': self.word_vocab['<NULL>'],
+        }
+        # collecting stack and buffer words into a single list
+        stack_buffer_list = [
+            state.stack[-i] if len(state.stack) >= i else 'NULL' for i in range(1,4)
+            ]+[state.buffer[-i] if len(state.buffer) >= i else 'NULL' for i in range(1,4)
+            ]
+         #create input vector based on items in stack_buffer_list
+        for i in range(6):
+            current_item= stack_buffer_list[i]
+            
+            if current_item == 'NULL':
+                input_vector[i]= special_symbols['NULL']
+            elif words[current_item] is None:
+                input_vector[i]= special_symbols['ROOT']
+            else:
+                words_lower_case= words[current_item].lower()
+                current_item_pos= pos[current_item]
+                
+                #using pos for cheking if each word is a special symbol
+                if current_item_pos in special_symbols:
+                    input_vector[i]= special_symbols[current_item_pos]
+                elif words_lower_case in self.word_vocab:
+                    input_vector[i]= self.word_vocab[words_lower_case]
+                else:
+                    input_vector[i]= special_symbols['UNK']
+                
+        return np.array(input_vector)
+
+    def get_output_representation(self, output_pair):  
+        # TODO: Write this method for Part 2
+        output_vector= np.zeros(91)
+        index = self.output_labels.get(output_pair)  # getting index from output_labels
+        if index is not None:  # If there is transition-lable pair in the dictionary
+            output_vector[index] = 1.0
+
+        return output_vector
+    
+def get_training_matrices(extractor, in_file):
+    inputs = []
+    outputs = []
+    count = 0 
+    for dtree in conll_reader(in_file): 
+        words = dtree.words()
+        pos = dtree.pos()
+        for state, output_pair in get_training_instances(dtree):
+            inputs.append(extractor.get_input_representation(words, pos, state))
+            outputs.append(extractor.get_output_representation(output_pair))
+        if count%100 == 0:
+            sys.stdout.write(".")
+            sys.stdout.flush()
+        count += 1
+    sys.stdout.write("\n")
+    return np.vstack(inputs),np.vstack(outputs)
+       
+
+
+if __name__ == "__main__":
+
+    WORD_VOCAB_FILE = 'data/words.vocab'
+    POS_VOCAB_FILE = 'data/pos.vocab'
+    #WORD_VOCAB_FILE = 'Downloads/NLP/HW2/hw2_torch/data/words.vocab'
+    #POS_VOCAB_FILE = 'Downloads/NLP/HW2/hw2_torch/data/pos.vocab'
+
+    try:
+        word_vocab_f = open(WORD_VOCAB_FILE,'r')
+        pos_vocab_f = open(POS_VOCAB_FILE,'r') 
+    except FileNotFoundError:
+        print("Could not find vocabulary files {} and {}".format(WORD_VOCAB_FILE, POS_VOCAB_FILE))
+        sys.exit(1) 
+
+
+    with open(sys.argv[1],'r') as in_file:   
+
+        extractor = FeatureExtractor(word_vocab_f, pos_vocab_f)
+        print("Starting feature extraction... (each . represents 100 sentences)")
+        inputs, outputs = get_training_matrices(extractor,in_file)
+        print("Writing output...")
+        np.save(sys.argv[2], inputs)
+        np.save(sys.argv[3], outputs)
+
+
---
+++ train_model.py
@@ -0,0 +1,132 @@
+import sys
+import numpy as np
+import torch
+
+from torch.nn import Module, Linear, Embedding, CrossEntropyLoss
+from torch.nn.functional import relu, log_softmax
+from torch.utils.data import Dataset, DataLoader 
+
+from extract_training_data import FeatureExtractor
+
+class DependencyDataset(Dataset):
+
+  def __init__(self, inputs_filename, output_filename):
+    self.inputs = np.load(inputs_filename)
+    self.outputs = np.load(output_filename)
+
+  def __len__(self): 
+    return self.inputs.shape[0]
+
+  def __getitem__(self, k): 
+    return (self.inputs[k], self.outputs[k])
+
+
+class DependencyModel(Module): 
+
+  def __init__(self, word_types, outputs):
+    super(DependencyModel, self).__init__()
+    # TODO: complete for part 3
+
+    #defining embedding layer
+    self.embedding = Embedding(num_embeddings=word_types, embedding_dim=128) 
+
+    #defining hidden layer
+    self.hidden_layer = Linear(in_features=768, out_features=128)
+
+    #defining output layer 
+    self.output_layer = Linear(in_features=128, out_features=outputs)
+
+  def forward(self, inputs):
+
+    # TODO: complete for part 3
+    embedded = self.embedding(inputs)  
+    
+    embedded_flattened = embedded.view(-1, 768) 
+    
+    #pass the concatenated embeddings into the hidden layer
+    hidden_output = relu(self.hidden_layer(embedded_flattened))
+    
+    #pass the result into the output layer
+    output= self.output_layer(hidden_output)
+    
+    return output
+
+
+def train(model, loader): 
+
+  #loss_function = NLLoss(reduction='mean')
+  loss_function = torch.nn.CrossEntropyLoss(reduction='mean')
+
+  LEARNING_RATE = 0.01 
+  optimizer = torch.optim.Adagrad(params=model.parameters(), lr=LEARNING_RATE)
+
+  tr_loss = 0 
+  tr_steps = 0
+
+  # put model in training mode
+  model.train()
+ 
+
+  correct = 0 
+  total =  0 
+  for idx, batch in enumerate(loader):
+ 
+    inputs, targets = batch
+    inputs=inputs.type(torch.LongTensor)
+    predictions = model(torch.LongTensor(inputs))
+
+    loss = loss_function(predictions, targets)
+    tr_loss += loss.item()
+
+    #print("Batch loss: ", loss.item()) # Helpful for debugging, maybe 
+
+    tr_steps += 1
+    
+    if idx % 1000==0:
+      curr_avg_loss = tr_loss / tr_steps
+      print(f"Current average loss: {curr_avg_loss}")
+
+    # To compute training accuracy for this epoch 
+    correct += sum(torch.argmax(predictions, dim=1) == torch.argmax(targets, dim=1))
+    total += len(inputs)
+      
+    # Run the backward pass to update parameters 
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+
+  epoch_loss = tr_loss / tr_steps
+  acc = correct / total
+  print(f"Training loss epoch: {epoch_loss},   Accuracy: {acc}")
+
+
+if __name__ == "__main__":
+
+    WORD_VOCAB_FILE = 'data/words.vocab'
+    POS_VOCAB_FILE = 'data/pos.vocab'
+    #WORD_VOCAB_FILE = 'Downloads/NLP/HW2/hw2_torch/data/words.vocab'
+    #POS_VOCAB_FILE = 'Downloads/NLP/HW2/hw2_torch/data/pos.vocab'
+
+    try:
+        word_vocab_f = open(WORD_VOCAB_FILE,'r')
+        pos_vocab_f = open(POS_VOCAB_FILE,'r')
+    except FileNotFoundError:
+        print("Could not find vocabulary files {} and {}".format(WORD_VOCAB_FILE, POS_VOCAB_FILE))
+        sys.exit(1)
+
+    extractor = FeatureExtractor(word_vocab_f, pos_vocab_f)
+
+    model = DependencyModel(len(extractor.word_vocab), len(extractor.output_labels))
+
+    dataset = DependencyDataset(sys.argv[1], sys.argv[2])
+    loader = DataLoader(dataset, batch_size = 16, shuffle = True)
+
+    print("Done loading data")
+
+    # Now train the model
+    for i in range(5): 
+      train(model, loader)
+
+
+    torch.save(model.state_dict(), sys.argv[3]) 
